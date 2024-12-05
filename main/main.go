58edbfa9464446a6d7b949b2ef43759c6e782788
hey-ewan <ewanretorokugbe@gmail.com>
2024-12-05 22:30:43 +0100

    set up google genai

--- main/main.go
+++ main/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
+	if err != nil {
+		panic(err)
+	}
+	model := client.GenerativeModel("gemini-1.5-pro")
+
+	resp, err := model.GenerateContent(ctx, genai.Text("Explain how AI works"))
+	fmt.Println(resp.Candidates[0].Content)
 }
